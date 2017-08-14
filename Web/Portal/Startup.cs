5d4b81d7c41e0bad00dc4ca384da4acda7eabf43
Matheus Santos <matheus.stag@gmail.com>
2017-08-14 14:09:07 -0300

    Adicionado projeto web Portal como início da migração para aspnet core 2.0

---
+++ Web/Portal/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProgramadorFajuto.Portal
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+    }
+}
