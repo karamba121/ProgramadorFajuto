6b09760e63b212e0289e7aa139f624bcb2ca00a6
Matheus Santos <matheus.stag@gmail.com>
2017-08-14 14:46:24 -0300

    Adicionado projetos para as camadas do projeto.

--- Web/Portal/Program.cs
+++ Web/Portal/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace ProgramadorFajuto.Portal
+namespace ProgramadorFajuto.Web.Portal
 {
     public class Program
     {
--- Web/Portal/Startup.cs
+++ Web/Portal/Startup.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProgramadorFajuto.Portal
+namespace ProgramadorFajuto.Web.Portal
 {
     public class Startup
     {
