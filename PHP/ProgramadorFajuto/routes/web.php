74b537a430cb4fbb1e5da27d99c6d41e16a93a19
matheus santos <matheus.stag@gmail.com>
2019-03-21 15:35:41 -0300

    ajustes

--- PHP/ProgramadorFajuto/routes/web.php
+++ PHP/ProgramadorFajuto/routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post', 'PostController@index')->name('post');
+Route::get('/ferramentas', 'FerramentasController@index')->name('ferramentas');
+Route::get('/login', 'LoginController@index')->name('login');
